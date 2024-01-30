d1abd83e67533c094cf34d0f5bfbc46caaa4bfe8
zcabeto <zcabeto@ucl.ac.uk>
2024-01-30 22:30:06 +0000

    amended password

--- app2/routes/db.js
+++ app2/routes/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'db-cont',
   user: 'root',
-  password: 'pass',
+  password: 'db-pass',
   database: 'MovieLens', 
   waitForConnections: true,
   connectionLimit: 10,
