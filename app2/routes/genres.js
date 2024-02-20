40f3adffd27568cd46f76706b01e49d8c2cb78fd
ghalaliagiga <ghalaliagiga@gmail.com>
2024-02-20 10:31:49 +0000

    changes

--- app2/routes/genres.js
+++ app2/routes/genres.js
@@ -1,24 +1,36 @@
 'use strict'
 var express = require('express');
 var router = express.Router();
-var pool = require('./db');     // retrieve pool from db.js
+var pool = require('./db');
 
-/* list genres of a chosen film */
+/* Display all genres */
 router.get('/', async function(req, res) {
   let connection;
-
   try {
     connection = await pool.getConnection();
-
-    const film = req.params.film;
-    // get list of genres from film
-    const getGenres = 'SELECT * FROM Genre;';
-    const [genres, fields] = await connection.execute(getGenres);     // get all the genres
-    // sort by average rating
-    res.render('genres', { title: 'Genres', data: genres });   // send data to response frontend
+    const sqlQuery = 'SELECT * FROM Genre;'; // Modify as needed for additional genre info
+    const [rows, fields] = await connection.execute(sqlQuery);
+    res.render('genres', { title: 'Genres', data: rows });
   } catch (err) {
     console.error('Error from genres/', err);
     res.render('error', { message: 'from genres/', error: err});
+  } finally {
+    if (connection) connection.release();
+  }
+});
+
+/* Search within genres */
+router.get('/search', async function(req, res) {
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    let searchQuery = req.query.searchQuery;
+    const sqlQuery = 'SELECT * FROM Genre WHERE name LIKE ?;'; // Adjust query for relevant genre attributes
+    const [rows, fields] = await connection.execute(sqlQuery, [`%${searchQuery}%`]);
+    res.render('genres', { title: 'Genres', data: rows });
+  } catch (err) {
+    console.error('Error from genres/search:', err);
+    res.render('error', { message: 'from genres/search', error: err});
   } finally {
     if (connection) connection.release();
   }
---
+++ app2/routes/ratings.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var pool = require('./db'); 
+
+// Get all ratings
+router.get('/', async function(req, res) {
+    
+});
+
+module.exports = router;
