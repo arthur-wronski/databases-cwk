395ce81d4cbc95b54430fb0277450a2ff298c7d4
zcabeto <zcabeto@ucl.ac.uk>
2024-02-20 14:28:41 +0000

    added films

--- app2/routes/filmInfo.js
+++ app2/routes/filmInfo.js
@@ -1,27 +1,35 @@
 'use strict'
 var express = require('express');
 var router = express.Router();
-var pool = require('./db'); // retrieve pool from db.js
-var InputSanitizer = require('./inputsanitizer'); // import sanitizer
+var pool = require('./db');     // retrieve pool from db.js
+
 
 /* show more info about the chosen film */
-router.get('/:userId', async function(req, res) {
+router.get('/:movieId', async function(req, res) {
   let connection;
 
   try {
     connection = await pool.getConnection();
+    const movieId = req.params.movieId;
 
-    // Sanitize the userId parameter before using it in the SQL query
-    const userId = InputSanitizer.sanitizeString(req.params.userId);
+    // select the chosen movie using its primary key
+    const getMovie = 'SELECT * FROM Movies WHERE movieId = ?;';
+    const [rowsMovie, fieldsM] = await connection.execute(getMovie, [movieId]);
+    const movie = rowsMovie[0]; // only one as primary
 
-    const sqlQuery = 'SELECT * FROM Viewer WHERE userId = ?;'; // select subset
-    const [rows, fields] = await connection.execute(sqlQuery, [userId]); // pooled connection to db
+    // select the genres of this movie
+    const getGenres = 'SELECT genreId, genre, ratingAv FROM Genres WHERE genreId IN ? ORDER BY rating;';
+    const [genres, fieldsG] = await connection.execute(getGenres, [movie.genres]);
 
-    // also need to get the viewers of the film and show them on this page. Click to show that viewers info??
-    res.render('filmInfo', { title: 'FilmInfo-' + userId, data: rows[0] }); // send data to response frontend
+    // select all the users who viewed this movie
+    const getViewers = 'SELECT userId, rating, rateTimestamp FROM Users WHERE movieRated = ?;';
+    const [viewers, fieldsV] = await connection.execute(getViewers, [movieId]);
+
+    // send output to response frontend
+    res.render('filmInfo', { title: 'FilmInfo: '+movie.title, movieData: movie, movieGenres: genres, movieViewers: viewers });
   } catch (err) {
-    console.error('Error from filmInfo/userId:', err);
-    res.render('error', { message: 'from filmInfo/userId', error: err});
+    console.error('Error from filmInfo/movieId:', err);
+    res.render('error', { message: 'from filmInfo/movieId', error: err});
   } finally {
     if (connection) connection.release();
   }
--- app2/routes/films.js
+++ app2/routes/films.js
@@ -10,9 +10,13 @@ router.get('/', async function(req, res) {
 
   try {
     connection = await pool.getConnection();
-    const sqlQuery = 'SELECT * FROM Viewer;';                   // select all
-    const [rows, fields] = await connection.execute(sqlQuery);  // pooled connection to db
-    res.render('films', { title: 'Films', data: rows });        // send output to response frontend
+
+    // get all movies
+    const getMovies = 'SELECT movieId, title, ratingAv FROM Movies;';
+    const [rows, fields] = await connection.execute(getMovies);
+
+    // send output to response frontend
+    res.render('films', { title: 'Films', data: rows });
   } catch (err) {
     console.error('Error from films/:', err);
     res.render('error', { message: 'from films/', error: err});
@@ -29,11 +33,26 @@ router.get('/search', async function(req, res) {
     connection = await pool.getConnection();
 
     // Sanitize the query parameter
-    let query = InputSanitizer.sanitizeString(req.query.query || '%');
+    let query = InputSanitizer.sanitizeString(req.query.searchQuery || '%');
 
-    const sqlQuery = 'SELECT * FROM Viewer WHERE movieId LIKE ?;';              // select subset
-    const [rows, fields] = await connection.execute(sqlQuery, [`%${query}%`]);  // pooled connection to db
-    res.render('films', { title: 'Films', data: rows });                        // send output to response frontend
+    // if search resembles a tag, movie results will be ordered by it
+    let related_tag = -1;
+    const search_tag = 'SELECT tagId FROM Tags WHERE tag LIKE ?;'
+    const [tags, fields] = await connection.execute(search_tag, [query]);
+    if (tags.length != 0){
+      related_tag = tags[0].tagId;
+    }
+
+    // find films resembling (in title or genre) the query and order by relation to chosen tag (if there is one)
+    if (related_tag != -1){
+      search_Movie_Genre = 'SELECT movieId, title, ratingAv FROM Movies WHERE title LIKE ? OR genre LIKE ? ORDER BY tags[?];';
+      const [movies, fields] = await connection.execute(search_Movie_Genre, [query, query, related_tag]);
+      res.render('films', { title: 'Films', data: movies });
+    } else {
+      search_Movie_Genre = 'SELECT movieId, title, ratingAv FROM Movies WHERE title LIKE ? OR genre LIKE ? ORDER BY tags[?];';
+      const [movies, fields] = await connection.execute(search_Movie_Genre, [query, query, related_tag]);
+      res.render('films', { title: 'Films', data: movies });
+    }
   } catch (err) {
     console.error('Error from films/search:', err);
     res.render('error', { message: 'from films/search', error: err});
