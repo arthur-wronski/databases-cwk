91615055c126bc93baeddb05955c99857ebb0846
zcabeto <zcabeto@ucl.ac.uk>
2024-02-25 15:33:05 +0000

    pick columns, link rows

--- app2/routes/films.js
+++ app2/routes/films.js
@@ -4,29 +4,7 @@ var router = express.Router();
 var pool = require('./db');     // retrieve pool from db.js
 var InputSanitizer = require('./inputsanitizer');
 
-/* show whole films table */
 router.get('/', async function(req, res) {
-  let connection;
-
-  try {
-    connection = await pool.getConnection();
-
-    // get all movies
-    const getMovies = 'SELECT movieId, title, ratingAv FROM Movies;';
-    const [rows, fields] = await connection.execute(getMovies);
-
-    // send output to response frontend
-    res.render('films', { title: 'Films', data: rows });
-  } catch (err) {
-    console.error('Error from films/:', err);
-    res.render('error', { message: 'from films/', error: err});
-  } finally {
-    if (connection) connection.release();
-  }
-});
-
-/* query-search the table for a subset */
-router.get('/search', async function(req, res) {
   let connection;
 
   try {
@@ -38,27 +16,56 @@ router.get('/search', async function(req, res) {
     // if search resembles a tag, movie results will be ordered by it
     let related_tag = -1;
     const search_tag = 'SELECT tagId FROM Tags WHERE tag LIKE ?;';
-    const [tags, fieldT] = await connection.execute(search_tag, [query]);
+    const [tags, fieldT] = await connection.execute(search_tag, [`%${query}%`]);
     if (tags.length != 0){
       related_tag = tags[0].tagId;
     }
 
     // find films resembling (in title or genre) the query and order by relation to chosen tag (if there is one)
+    let search_Movie_Genre; let movies; let fields;
     if (related_tag != -1){
-      search_Movie_Genre = 'SELECT movieId, title, ratingAv FROM Movies WHERE title LIKE ? OR genre LIKE ? ORDER BY tags[?];';
-      const [movies, fieldM] = await connection.execute(search_Movie_Genre, [query, query, related_tag]);
-      res.render('films', { title: 'Films', data: movies });
+      search_Movie_Genre = `SELECT * FROM Movies WHERE title LIKE ${query} OR genre LIKE ${query};`;
+      [movies, fields] = await connection.execute(search_Movie_Genre);
     } else {
-      search_Movie_Genre = 'SELECT movieId, title, ratingAv FROM Movies WHERE title LIKE ? OR genre LIKE ? ORDER BY tags[?];';
-      const [movies, fieldM] = await connection.execute(search_Movie_Genre, [query, query, related_tag]);
-      res.render('films', { title: 'Films', data: movies });
+      search_Movie_Genre = `SELECT * FROM Movies WHERE title LIKE ${query} OR genre LIKE ${query} ORDER BY tags[?];`;
+      [movies, fields] = await connection.execute(search_Movie_Genre, [`%${related_tag}%`]);
     }
+
+    // set the used columns
+    const shownColQuery = req.query.shownCols;
+    const colQuery = req.query.col;
+    let shownCols;
+    const allCols = fields.map(field => field.name);
+    if (shownColQuery==null) shownCols = allCols;
+    else shownCols = shownColQuery.split(',');
+    if (colQuery!=null) shownCols = add_or_remove(allCols, shownCols, colQuery);
+
+    // render the data
+    res.render('films', { title: 'Films', data: rows, allCols: allCols, shownCols: shownCols});
   } catch (err) {
-    console.error('Error from films/search:', err);
-    res.render('error', { message: 'from films/search', error: err});
+    console.error('Error from films/:', err);
+    res.render('error', { message: 'from films/', error: err});
   } finally {
     if (connection) connection.release();
   }
 })
+
+function add_or_remove(allEls, shownEls, element){
+  const index = shownEls.indexOf(element);
+  if (index == -1){
+    // re-add the element in its original position
+    let newShown = [];
+    for (var i=0; i<allEls.length; i++){
+      if (shownEls.includes(allEls[i]) || allEls[i]==element){
+        newShown.push(allEls[i]);
+      }
+    }
+    return newShown;
+  } else {
+    // remove the element
+    shownEls.splice(index, 1);
+    return shownEls;
+  }
+}
 
 module.exports = router;
