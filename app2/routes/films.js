99ae43b3397fa9b62ae6beb717dc7e99a57a3d1e
zcabeto <zcabeto@ucl.ac.uk>
2024-02-25 15:18:53 +0000

    pick columns

--- app2/routes/films.js
+++ app2/routes/films.js
@@ -10,9 +10,22 @@ router.get('/', async function(req, res) {
 
   try {
     connection = await pool.getConnection();
-    const sqlQuery = 'SELECT * FROM Viewer;';                   // select all
-    const [rows, fields] = await connection.execute(sqlQuery);  // pooled connection to db
-    res.render('films', { title: 'Films', data: rows });        // send output to response frontend
+    // handle any search queries if entered
+    let query = InputSanitizer.sanitizeString(req.query.query || '%');
+    const sqlQuery = `SELECT * FROM Viewer WHERE movieId LIKE ?;`;
+    const [rows, fields] = await connection.execute(sqlQuery, [`%${query}%`]);
+
+    // set the used columns
+    const shownColQuery = req.query.shownCols;
+    const colQuery = req.query.col;
+    let shownCols;
+    const allCols = fields.map(field => field.name);
+    if (shownColQuery==null) shownCols = allCols;
+    else shownCols = shownColQuery.split(',');
+    if (colQuery!=null) shownCols = add_or_remove(allCols, shownCols, colQuery);
+    
+    // render the data
+    res.render('films', { title: 'Films', data: rows, allCols: allCols, shownCols: shownCols});
   } catch (err) {
     console.error('Error from films/:', err);
     res.render('error', { message: 'from films/', error: err});
@@ -21,25 +34,22 @@ router.get('/', async function(req, res) {
   }
 });
 
-/* query-search the table for a subset */
-router.get('/search', async function(req, res) {
-  let connection;
-
-  try {
-    connection = await pool.getConnection();
-
-    // Sanitize the query parameter
-    let query = InputSanitizer.sanitizeString(req.query.query || '%');
-
-    const sqlQuery = 'SELECT * FROM Viewer WHERE movieId LIKE ?;';              // select subset
-    const [rows, fields] = await connection.execute(sqlQuery, [`%${query}%`]);  // pooled connection to db
-    res.render('films', { title: 'Films', data: rows });                        // send output to response frontend
-  } catch (err) {
-    console.error('Error from films/search:', err);
-    res.render('error', { message: 'from films/search', error: err});
-  } finally {
-    if (connection) connection.release();
+function add_or_remove(allEls, shownEls, element){
+  const index = shownEls.indexOf(element);
+  if (index == -1){
+    // re-add the element in its original position
+    let newShown = [];
+    for (var i=0; i<allEls.length; i++){
+      if (shownEls.includes(allEls[i]) || allEls[i]==element){
+        newShown.push(allEls[i]);
+      }
+    }
+    return newShown;
+  } else {
+    // remove the element
+    shownEls.splice(index, 1);
+    return shownEls;
   }
-})
+}
 
 module.exports = router;
