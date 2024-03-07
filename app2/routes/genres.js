b4d5e2beaf17cdd03d4f26548d4aa3eb8aaf33f1
zcabeto <zcabeto@ucl.ac.uk>
2024-03-07 19:25:51 +0000

    genres simplify

--- app2/routes/genres.js
+++ app2/routes/genres.js
@@ -2,36 +2,20 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('./db');
-var InputSanitizer = require('./inputsanitizer');
+var InputSanitizer = require('./inputsanitizer'); // import sanitizer
+
 
 /* Display all genres */
 router.get('/', async function(req, res) {
   let connection;
   try {
     connection = await pool.getConnection();
-    const sqlQuery = 'SELECT * FROM Genres;'; // Modify as needed for additional genre info
-    const [rows, fields] = await connection.execute(sqlQuery);
-    res.render('genres', { title: 'Genres', data: rows });
+    const getGenres = 'SELECT * FROM Genres;';
+    const [genres, fields] = await connection.execute(getGenres);
+    res.render('genres', { title: 'Genres', genres: genres });
   } catch (err) {
     console.error('Error from genres/', err);
     res.render('error', { message: 'from genres/', error: err});
-  } finally {
-    if (connection) connection.release();
-  }
-});
-
-/* Search within genres */
-router.get('/search', async function(req, res) {
-  let connection;
-  try {
-    connection = await pool.getConnection();
-    let query = InputSanitizer.sanitizeString(req.query.searchQuery || '%');
-    const sqlQuery = 'SELECT * FROM Genres WHERE genre LIKE ?;'; // Adjust query for relevant genre attributes
-    const [rows, fields] = await connection.execute(sqlQuery, [query]);
-    res.render('genres', { title: 'Genres', data: rows });
-  } catch (err) {
-    console.error('Error from genres/search:', err);
-    res.render('error', { message: 'from genres/search', error: err});
   } finally {
     if (connection) connection.release();
   }
