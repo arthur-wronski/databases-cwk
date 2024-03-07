39e7469ef223bf51d109c3e2698a484820feccd4
zcabeto <zcabeto@ucl.ac.uk>
2024-03-07 19:37:36 +0000

    new date format

--- app2/routes/userInfo.js
+++ app2/routes/userInfo.js
@@ -14,8 +14,17 @@ router.get('/:userId', async function(req, res) {
     let userId = InputSanitizer.sanitizeString(req.params.userId || '%');
     
     // only take subset to improve processing
-    let getRatings = `SELECT Viewer.movieId, Movies.title, Viewer.rating, Viewer.timestamp FROM Viewer INNER JOIN Movies ON (Viewer.movieId=Movies.movieId) WHERE userId=?;`;
+    let getRatings = `SELECT Viewer.movieId, Movies.title, Viewer.rating, Viewer.watchDate FROM Viewer INNER JOIN Movies ON (Viewer.movieId=Movies.movieId) WHERE userId=?;`;
     let [ratings, fields] = await connection.execute(getRatings, [`${userId}`]);
+
+    ratings.forEach(rating => {
+      if (rating.watchDate) {
+        // Converting date to dd/mm/yyyy format
+        const date = new Date(rating.watchDate);
+        const formattedDate = date.toLocaleDateString('en-GB'); // 'en-GB' uses dd/mm/yyyy format
+        rating.watchDate = formattedDate;
+      }
+    });
 
     // render the data
     res.render('userInfo', { title: 'User '+userId, ratings: ratings });
