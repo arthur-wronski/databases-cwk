46bb5239046073f0bd5cdd47489e17b548a451a4
ghalaliagiga <ghalaliagiga@gmail.com>
2024-03-12 17:21:45 +0000

    prediction

--- app2/routes/predict.js
+++ app2/routes/predict.js
@@ -1,9 +1,21 @@
-
 'use strict';
 var express = require('express');
 var router = express.Router();
 var pool = require('./db');
 var InputSanitizer = require('./inputsanitizer');
+
+function add_or_remove(selectedTags, tag) {
+  const index = selectedTags.indexOf(tag);
+
+  if (index === -1) {
+    selectedTags.push(tag);
+  } else {
+    selectedTags.splice(index, 1);
+  }
+
+  return selectedTags;
+}
+
 
 router.get('/', async function(req, res) {
   let connection;
@@ -14,52 +26,34 @@ router.get('/', async function(req, res) {
     let itemNum = parseInt(InputSanitizer.sanitizeString(req.query.itemNum || '0'));
     if (itemNum < 0) itemNum = 0;
 
-    let getTags = `SELECT * FROM Tags WHERE tagId LIKE ? LIMIT ?,30;`;
+    let getTags = `SELECT * FROM Tags WHERE tag LIKE ? LIMIT ?,30;`;
     let [tags, fieldsT] = await connection.execute(getTags, [`%${searchQuery}%`, `${itemNum}`]);
 
-    const query = `
-      SELECT Viewer.rating, Tags.tagId
-      FROM Tags NATURAL JOIN Viewer;
-    `;
-    
-    const [ratings, fieldV] = await connection.execute(query);
+    let selectedTagsQuery = req.query.selectedTags || '';
+    let selectedTags = [...new Set(selectedTagsQuery ? selectedTagsQuery.split(',') : [])];
 
-    // we cant be cool
-    // we need to get all the ratings and for each tag get their average and sd
-    // do the thing below this comment in a for loop for each of the tags in the tag list xoxo gossip girl 
-    // for loop to go thourh each tag either with a dictoinary or a 2d array - EACH TAG HAS A CORRESPONDING EXP AND SD 
-    // ALSO COMBINED ONES - PUT TOGETHER THE ONES YOUVE SPECIFCAKH GOTON
-    // ARRAY OF TAGS
+    let averageRating; 
+    if (req.query.tag) {
+      selectedTags = add_or_remove(selectedTags, req.query.tag);
 
-    // dictionary
-    let tagAverages = {
-      [tagId]: { exp: averageRating, sdev: standardDeviation }
-    }
-
-    for (let i = 0; i < tags.length; i++) {
-        let tagId = tags[i].tagId;
-
-        let sum = 0, sumSq = 0, n = ratings.length;
-        ratings.forEach(rating => {
-            let value = parseFloat(rating.rating);
-            sum += value;
-            sumSq += value * value;
-        });
-        
-        let mean = n > 0 ? sum / n : 0;
-        let variance = n > 0 ? (sumSq / n - mean * mean) : 0;
-        let sd = Math.sqrt(variance);
-
-        tagAverages[tagId] = { mean, sd };
-    }
+      let avgRatingQuery = `
+        SELECT AVG(v.rating) as averageRating
+        FROM Viewer v
+        JOIN Tags t ON v.userId = t.userId AND v.movieId = t.movieId
+        WHERE t.tag = ?;
+      `;
+      let [avgRatingResult, fieldsV] = await connection.execute(avgRatingQuery, [req.query.tag]);
+      averageRating = avgRatingResult[0]?.averageRating; 
 
     res.render('predict', { 
         title: 'Predictions', 
         tags: tags, 
+        selectedTags: selectedTags,
         searchQuery: searchQuery,
         itemNum: itemNum,
-        tagAverages: tagAverages 
+        averageRating: averageRating ? averageRating.toFixed(1) : 'No ratings' 
     });
+    } 
   } catch (err) {
     console.error('Error from predictions/', err);
     res.render('error', { message: 'Error loading predictions', error: err });
