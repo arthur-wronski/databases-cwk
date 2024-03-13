b369285503d5d38f8dfc4bd9c03f1f7e2e26581a
zcabeto <zcabeto@ucl.ac.uk>
2024-03-13 09:25:53 +0000

    show line & points together

--- app2/routes/personality_genre.js
+++ app2/routes/personality_genre.js
@@ -15,17 +15,17 @@ router.get('/:genreTrait', async function(req, res) {
 
     const genreId = parseInt(InputSanitizer.sanitizeString(genre));
     let getGenre = `
-        SELECT * 
+        SELECT *
         FROM Genres
     ;`;
-    let [genres, fieldsG] = await connection.execute(getGenre, [`${genreId}`]);
+    let [genres, fieldsG] = await connection.execute(getGenre);
     const currentGenre = genres[genreId];
 
     const traits = ['openness',  'agreeableness', 'emotional_stability', 'conscientiousness', 'extraversion'];
     const currentTrait = InputSanitizer.sanitizeString(trait);
-    /*
+    /* 
     let getTraitVals = `
-      SELECT Personality.? AS traitVal, ThisGenreRatings.rating
+      SELECT Personality.${currentTrait} AS traitVal, ThisGenreRatings.rating
       FROM Personality 
       NATURAL JOIN 
       (SELECT PersonalityRatings.userId, PersonalityRatings.rating 
@@ -33,11 +33,11 @@ router.get('/:genreTrait', async function(req, res) {
         NATURAL JOIN
         (SELECT * FROM MovieGenres WHERE genreId = ?) AS ThisGenreMovies
       ) AS ThisGenreRatings
-      ORDER BY Personality.?;
+      ORDER BY Personality.${currentTrait};
     `;
-    let [traitVals, fieldsTV] = await connection.execute(getTraitVals, [`${currentTrait}`, `${currentGenre.genreId}`, `${currentTrait}`]);
+    let [traitVals, fieldsTV] = await connection.execute(getTraitVals, [`${currentGenre.genreId}`]);
     */
-    let traitRate = [{traitVal: 0.6, rating: 4}, {traitVal: 1, rating: 1}, {traitVal: 2, rating: 3}];
+    let traitRate = [{traitVal: 0.5, rating: 4}, {traitVal: 1, rating: 1}, {traitVal: 2, rating: 3}];
     const traitRateVars = {traitVal: 'metric', rating: 'metric'};
     const stats = new Statistics(traitRate, traitRateVars);
     const regression = stats.linearRegression('traitVal', 'rating');
@@ -47,8 +47,8 @@ router.get('/:genreTrait', async function(req, res) {
         currentGenre: currentGenre, currentTrait: currentTrait,
         regression: {coefficient: regression.correlationCoefficient.toPrecision(3), 
           pointStart: regression.regressionSecond.beta1, pointEnd: 10*regression.regressionSecond.beta2 + regression.regressionSecond.beta1},
-        data: traitRate.map(pair => [pair.traitVal,pair.rating,'X']),
-        traitVals: traitVals
+        data: traitRate.map(pair => [pair.traitVal,pair.rating,'X'])
+        //traitVals: traitVals
     })
 
   } catch (err) {
